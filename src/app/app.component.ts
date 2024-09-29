935e0217f185435fe4544e25ee8431a247aea8b3
Deividas Kondratjevas <deividaskond@gmail.com>
2024-09-29 14:37:33 +0200

    start why-me c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { WhyMeComponent } from './components/why-me/why-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutMeComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
