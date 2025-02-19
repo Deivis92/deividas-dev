4d30bb42fa7275adb14d55ee77c8f4c005eaea71
Deividas Kondratjevas <deividaskondgmail.com>
2025-02-19 09:20:33 +0100

    teamplayer translation

--- src/app/components/teamplayer/teamplayer.component.ts
+++ src/app/components/teamplayer/teamplayer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Comment } from '../../models/comments.model';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-teamplayer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './teamplayer.component.html',
   styleUrl: './teamplayer.component.scss'
 })
@@ -13,21 +14,18 @@ export class TeamplayerComponent {
   comments: Comment[] = [{
     name: 'Noah Müller',
     title: 'Project Join',
-    description: 'Deividas had to develop,format and deliver content in collaboartion with the team members.He is a reliable and friendly person.',
+    description: 'teamplayer.JOIN'
   },
+
   {
     name: 'Evelyn Marx',
     title: 'El Pollo Loco',
-    description: 'He is a trustworthy teamplayer and can cope with the stress of deadlines. Structured work and clear code.',
+    description: 'teamplayer.LOCO'
   },
   {
     name: 'James Rugman',
     title: 'DABubble',
-    description: 'He is reliable and friendly person. Work in a structured way and write a clear code. I recommend him as a colleague.',
+    description: 'teamplayer.DABUBLE',
   }
-
-
-
   ];
-
 }
