45afab82a6fc15db80e9e67abe0e1099fbeaed9a
Deividas Kondratjevas <deividaskondgmail.com>
2025-03-04 12:06:43 +0100

    contact formated

--- src/app/landing-page/contact-me/contact-me.component.ts
+++ src/app/landing-page/contact-me/contact-me.component.ts
@@ -2,19 +2,19 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar'; // Import MatSnackBar
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [CommonModule, FormsModule, TranslateModule, MatSnackBarModule], // Add MatSnackBarModule
+  imports: [CommonModule, FormsModule, TranslateModule, MatSnackBarModule],
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss'],
 })
 export class ContactMeComponent {
   http = inject(HttpClient);
-  snackBar = inject(MatSnackBar); // Inject MatSnackBar
+  snackBar = inject(MatSnackBar);
 
   contactData = {
     name: '',
@@ -39,53 +39,57 @@ export class ContactMeComponent {
   showPrivacyError = false;
 
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && this.isPolicyAccepted) {
-      const formData = {
-        name: this.contactData.name,
-        email: this.contactData.email,
-        message: this.contactData.message,
-      };
-  
-      console.log('Sending form data:', formData);
-  
-      this.http
-        .post(this.post.endPoint, this.post.body(formData), this.post.options)
-        .subscribe({
-          next: (response) => {
-            console.log('Form successfully submitted:', response);
-            this.showSnackBar('Thank you for your message. I will get back to you shortly.', 'success');
-            
-            // Alle Felder nach erfolgreichem Absenden leeren
-            this.contactData = {
-              name: '',
-              email: '',
-              message: '',
-            };
-            
-            // Formular zurücksetzen
-            ngForm.resetForm();
-          },
-          error: (error) => {
-            console.error('Error during form submission:', error);
-          },
-          complete: () => {
-            console.log('Request completed.');
-          },
-        });
-    } else if (!this.isPolicyAccepted) {
-      console.log('Privacy policy must be accepted.');
+    if (!ngForm.valid || !this.isPolicyAccepted) {
+      if (!this.isPolicyAccepted) this.showPrivacyError = true;
+      return;
     }
+
+    this.sendFormData(ngForm);
   }
-  
+
+  sendFormData(ngForm: NgForm): void {
+    const formData = this.createFormData();
+    this.http
+      .post(this.post.endPoint, this.post.body(formData), this.post.options)
+      .subscribe({
+        next: () => this.handleSuccess(ngForm),
+        error: (error) => this.handleError(error),
+      });
+  }
+
+  createFormData() {
+    return {
+      name: this.contactData.name,
+      email: this.contactData.email,
+      message: this.contactData.message,
+    };
+  }
+
+  handleSuccess(ngForm: NgForm): void {
+    this.showSnackBar('Thank you for your message. I will get back to you shortly.', 'success');
+    this.resetFormData();
+    ngForm.resetForm();
+  }
+
+  handleError(error: any): void {
+    console.error('Error during form submission:', error);
+  }
+
+  resetFormData(): void {
+    this.contactData = {
+      name: '',
+      email: '',
+      message: '',
+    };
+  }
 
   showSnackBar(message: string, type: string): void {
-    this.snackBar.open(message, '', {  // Leere Zeichenfolge für den Close-Button
-      duration: 3000,  // Dauer in Millisekunden
-      horizontalPosition: 'center',  // Position horizontal in der Mitte
-      verticalPosition: 'top',       // Position vertikal oben
-      panelClass: type,              // 'success' oder 'error' für spezifische Styles
+    this.snackBar.open(message, '', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: type,
     });
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
-
