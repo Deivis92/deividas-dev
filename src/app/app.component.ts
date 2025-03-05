5cc2f20d4a8ce7957ce2495c6d6dd08c703fcf39
Deividas Kondratjevas <deividaskondgmail.com>
2025-03-05 10:52:18 +0100

    aos added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainContentComponent } from './landing-page/main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint/imprint.component';
+import AOS from "aos";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, MainContentComponent, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'deividas-dev';
+
+  ngOnInit() {
+    AOS.init();
+  }
 }
--- src/app/landing-page/main-content/main-content.component.ts
+++ src/app/landing-page/main-content/main-content.component.ts
@@ -1,28 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { WhyMeComponent } from '../why-me/why-me.component';
 import { MySkillsComponent } from '../my-skills/my-skills.component';
 import { MyProjectsComponent } from '../my-projects/my-projects.component';
 import { TeamplayerComponent } from '../teamplayer/teamplayer.component';
 import { ContactMeComponent } from '../contact-me/contact-me.component';
+import AOS from "aos";
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   imports: [
-    CommonModule, 
-    AboutMeComponent, 
-    WhyMeComponent, 
-    MySkillsComponent, 
-    MyProjectsComponent, 
-    TeamplayerComponent, 
+    CommonModule,
+    AboutMeComponent,
+    WhyMeComponent,
+    MySkillsComponent,
+    MyProjectsComponent,
+    TeamplayerComponent,
     ContactMeComponent
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit {
 
+  ngOnInit() {
+    AOS.init();
+  }
 }
